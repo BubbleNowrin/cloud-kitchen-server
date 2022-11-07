281e5ad3cd35b7735778bec7cb7fa4155bfce009
NowrinIslamMim <nowrin02@gmail.com>
2022-11-08 04:42:01 +0600

    food details for specific id api created

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -21,8 +21,17 @@ async function run() {
 
     try {
         const foodCollection = client.db('foodaholic').collection('foodData');
+
         app.get('/foods', async (req, res) => {
             const query = {};
+            const cursor = foodCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        app.get('/foods/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
             const cursor = foodCollection.find(query);
             const result = await cursor.toArray();
             res.send(result);
