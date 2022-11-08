c00fb0c9117ced4fd4004281806c1d5eb4aa376b
NowrinIslamMim <nowrin02@gmail.com>
2022-11-09 03:09:16 +0600

    update review api created

--- index.js
+++ index.js
@@ -67,6 +67,15 @@ async function run() {
             res.send(reviews);
         })
 
+        app.get('/update/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = {
+                _id: ObjectId(id)
+            };
+            const result = await reviewsCollection.findOne(query);
+            res.send(result);
+        })
+
         app.post('/reviews/:id', async (req, res) => {
             const review = req.body;
             const result = await reviewsCollection.insertOne(review);
