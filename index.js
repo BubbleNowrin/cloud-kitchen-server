b688c8e4aba7cfba49fd09533e1c80d4c9a59c2d
NowrinIslamMim <nowrin02@gmail.com>
2022-11-10 02:24:45 +0600

    comment edited

--- index.js
+++ index.js
@@ -7,17 +7,17 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 
-//middleware
+// middleware
 app.use(cors());
 app.use(express.json());
 
-console.log(process.env.DB_PASSWORD);
-console.log(process.env.DB_USER);
-
+//mongodb connect
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ng69xjx.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+
+//jwt implementation
 const verifyJWT = (req, res, next) => {
     const authHeader = req.headers.authorization;
     if (!authHeader) {
